654d1db2eaac443c84469e6957b443a2ed630424
Milan Gallo <gallo.milan.jr@gmail.com>
2020-05-26 23:06:22 +0200

    Blog content interlude
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- arch/i386/boot/src/boot.s
+++ arch/i386/boot/src/boot.s
@@ -14,21 +14,21 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-# Declare constants for the multiboot header.
+# Declare constants for the Multiboot header
 .set ALIGN,    1<<0             # align loaded modules on page boundaries
 .set MEMINFO,  1<<1             # provide memory map
 .set FLAGS,    ALIGN | MEMINFO  # this is the Multiboot 'flag' field
 .set MAGIC,    0x1BADB002       # 'magic number' lets bootloader find the header
 .set CHECKSUM, -(MAGIC + FLAGS) # checksum of above, to prove we are multiboot
 
-# Declare a header as in the Multiboot Standard.
+# Declare a header as in the Multiboot Standard
 .section .multiboot
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
-# Reserve a stack for the initial thread.
+# Reserve a stack for the initial thread
 .section .bss
 
 .align 2
@@ -39,14 +39,15 @@ xos_reg_dump_top:
 
 .align 16
 stack_bottom:
-.skip 16384 # 16 KiB
+.skip 16384 # 16 kB
 stack_top:
 
-# The kernel entry point.
+# The kernel entry point
 .section .text
 .global _start
 .type _start, @function
 _start:
+    # Dump all segment registers for examination
     movl $xos_reg_dump_top, %esp
     movw %gs, %ax
     pushw %ax
@@ -61,18 +62,19 @@ _start:
     movw %cs, %ax
     pushw %ax
 
+    # Set up stack
 	movl $stack_top, %esp
 
-	# Call the global constructors.
+	# Call the global constructors
 	# Ignored for now
 	# call _init
 
-	# Transfer control to the main kernel.
+	# Transfer control to the main kernel
 	push %ebx
 	push %eax
 	call kernel_main
 
-	# Hang if kernel_main unexpectedly returns.
+	# Hang if kernel_main unexpectedly returns
 	cli
 1:	hlt
 	jmp 1b
