6b7831a6b3d628ca65a522ee6d5d2c9ba7bb1add
Milan Gallo <gallo.milan.jr@gmail.com>
2022-01-23 15:20:21 +0100

    Add x64 boot
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

---
+++ arch/x86_64/boot/src/start.cpp
@@ -0,0 +1,121 @@
+// xos
+// Copyright (C) 2020 Milan Gallo <gallo.milan.jr@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+// Created by Milan Gallo on 25/12/2021.
+//
+
+#include <start.h>
+
+#include <xos/attributes.h>
+#include <cstdint>
+#include <xos/string.h>
+#include "stivale2.h"
+
+
+// We need to tell the stivale bootloader where we want our stack to be.
+// We are going to allocate our stack as an array in .bss.
+static uint8_t stack[8192];
+
+using namespace Stivale_v2;
+
+// stivale2 uses a linked list of tags for both communicating TO the
+// bootloader, or receiving info FROM it. More information about these tags
+// is found in the stivale2 specification.
+
+// stivale2 offers a runtime terminal service which can be ditched at any
+// time, but it provides an easy way to print out to graphical terminal,
+// especially during early boot.
+// Read the notes about the requirements for using this feature below this
+// code block.
+static struct Stivale::Header::Terminal terminal_hdr_tag = {
+        .tag = {
+                .id = Stivale::Header::TagId::Terminal,
+                .next = nullptr
+        },
+        .flags = 0
+};
+
+// We are now going to define a framebuffer header tag.
+// This tag tells the bootloader that we want a graphical framebuffer instead
+// of a CGA-compatible text mode. Omitting this tag will make the bootloader
+// default to text mode, if available.
+static struct Stivale::Header::Framebuffer framebuffer_hdr_tag = {
+        .tag = {
+                .id = Stivale::Header::TagId::Framebuffer,
+                .next = &terminal_hdr_tag.tag,
+        },
+        .framebufferWidth  = 0,
+        .framebufferHeight = 0,
+        .framebufferBitsPerPixel = 0
+};
+
+// The stivale2 specification says we need to define a "header structure".
+// This structure needs to reside in the .stivale2hdr ELF section in order
+// for the bootloader to find it. We use this __attribute__ directive to
+// tell the compiler to put the following structure in said section.
+_Section(.stivale2hdr)
+static struct Stivale::Header::Struct stivale_hdr = {
+        .entryPoint = 0,
+        .stack = uintptr_t(stack) + sizeof(stack),
+        // Bit 1, if set, causes the bootloader to return to us pointers in the
+        // higher half, which we likely want since this is a higher half kernel.
+        // Bit 2, if set, tells the bootloader to enable protected memory ranges,
+        // that is, to respect the ELF PHDR mandated permissions for the executable's
+        // segments.
+        // Bit 3, if set, enables fully virtual kernel mappings, which we want as
+        // they allow the bootloader to pick whichever *physical* memory address is
+        // available to load the kernel, rather than relying on us telling it where
+        // to load it.
+        // Bit 4 disables a deprecated feature and should always be set.
+        .flags = (0 << 1) | (1 << 2) | (1 << 3) | (1 << 4),
+        .tags = &framebuffer_hdr_tag.tag,
+};
+
+// We will now write a helper function which will allow us to scan for tags
+// that we want FROM the bootloader (structure tags).
+const Stivale::Struct::Tag *stivaleGetTag(const Stivale::Struct::Struct *stivale, uint64_t id) {
+        for (auto walk = stivale->tags; walk; walk = walk->next) {
+                if (walk->id == id)
+                        return walk;
+        }
+
+        return nullptr;
+}
+
+void _start(const Stivale::Struct::Struct *stivale) {
+        auto terminal = static_cast<const Stivale::Struct::Terminal *>(
+                stivaleGetTag(stivale, Stivale::Struct::TagId::Terminal)
+        );
+
+        if (!terminal) {
+                _forever
+                        asm ("hlt");
+        }
+
+        auto write = terminal->write;
+
+        // We should now be able to call the above function pointer to print out
+        // a simple "Hello World" to screen.
+        const auto message =
+                "xos operating system v0.1.0\n"
+                "Copyright (C) 2020 Milan Gallo <gallo.milan.jr@gmail.com>\n"
+                "\n";
+        write(message, xos::strlen(message));
+
+        // We're done, just hang...
+        _forever
+                asm ("hlt");
+}
---
+++ arch/x86_64/boot/src/stivale2.h
@@ -0,0 +1,357 @@
+// xos
+// Copyright (C) 2020 Milan Gallo <gallo.milan.jr@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+// Created by Milan Gallo on 24/12/2021.
+//
+
+#ifndef _XOS_SRC_STIVALE2_H
+#define _XOS_SRC_STIVALE2_H
+
+#include <cstdint>
+
+namespace Stivale_v2::Stivale {
+        // Anchor for non ELF kernels
+        struct Anchor {
+                uint8_t anchor[15];
+                uint8_t bits;
+                uintptr_t physicalLoadAddress,
+                        physicalBssStart,
+                        physicalBssEnd,
+                        physicalHeader;
+        };
+
+        template<typename Id>
+        struct Tag {
+                Id id;
+                Tag<Id> *next;
+        };
+
+        /* --- Header --------------------------------------------------------------- */
+        /*  Information passed from the kernel to the bootloader                      */
+        namespace Header {
+                namespace TagId {
+                        enum Enum : uint64_t {
+                                Null = 0u,
+                                AnyVideo = 0xc75c9fa92a44c4dbu,
+                                Framebuffer = 0x3ecc1bc43d0f7971u,
+                                MTRR = 0x4c7bb07731282e00u,
+                                SlideHHDM = 0xdc29269c2af53d1du,
+                                Terminal = 0xa85d499b1823be72u,
+                                SMP = 0x1ab015085f3273dfu,
+                                Paging5Lvl = 0x932f477032007e8fu,
+                                UnmapNull = 0x92919432b16fe7e7u,
+                        };
+                }
+
+                using Tag = Stivale::Tag<TagId::Enum>;
+
+                struct Struct {
+                        uintptr_t entryPoint;
+                        uintptr_t stack;
+                        uint64_t flags;
+                        Tag *tags;
+                };
+
+                struct AnyVideo {
+                        Tag tag;
+                        uint64_t preference;
+                };
+
+                struct Framebuffer {
+                        Tag tag;
+                        uint16_t framebufferWidth;
+                        uint16_t framebufferHeight;
+                        uint16_t framebufferBitsPerPixel;
+                        uint16_t unused = 0;
+                };
+
+                using MTRR = Tag;
+
+                struct SlideHHDM {
+                        Tag tag;
+                        uint64_t flags;
+                        uint64_t alignment;
+                };
+
+                struct Terminal {
+                        enum CallbackType : uint64_t {
+                                DEC = 10u,
+                                Bell = 20u,
+                                PrivateId = 30u,
+                                StatusReport = 40u,
+                                PositionReport = 50u,
+                                KeyboardLeds = 60u,
+                                Mode = 70u,
+                                Linux = 80u,
+                        };
+
+                        using Callback = void (CallbackType, uint64_t, uint64_t, uint64_t);
+
+                        Tag tag;
+                        uint64_t flags;
+                        Callback *callback = nullptr;
+                };
+
+                struct SMP {
+                        Tag tag;
+                        uint64_t flags;
+                };
+
+                using Paging5Lvl = Tag;
+                using UnmapNull = Tag;
+        }
+
+        /* --- Struct --------------------------------------------------------------- */
+        /*  Information passed from the bootloader to the kernel                      */
+        namespace Struct {
+                namespace TagId {
+                        enum Enum : uint64_t {
+                                PMRs = 0x5df266a64047b6bdu,
+                                KernelBaseAddress = 0x060d78874a2a8af0u,
+                                CommandLine = 0xe5e76a1b4597a781u,
+                                MemoryMap = 0x2187f79e8612de07u,
+                                Framebuffer = 0x506461d2950408fau,
+                                EDID = 0x968609d7af96b845u,
+                                TextMode = 0x38d74c23e0dca893u,
+                                MTRR = 0x6bc1a78ebe871172u,
+                                Terminal = 0xc2b3f4c3233b0974u,
+                                Modules = 0x4b6fe466aade04ceu,
+                                RSDP = 0x9e1786930a375e78u,
+                                Epoch = 0x566a7bed888e1407u,
+                                Firmware = 0x359d837855e3858cu,
+                                EFISystemTable = 0x4bc5ec15845b558eu,
+                                KernelFile = 0xe599d90c2975584au,
+                                KernelFile_v2 = 0x37c13018a02c6ea2u,
+                                BootVolume = 0x9b4358364c19ee62u,
+                                KernelSlide = 0xee80847d01506c57u,
+                                SMBIOS = 0x274bd246c62bf7d1u,
+                                SMP = 0x34d1d96339647025u,
+                                PXEServerInfo = 0x29d1e96239247032u,
+                                MMIO32UART = 0xb813f9b8dbc78797u,
+                                DeviceTreeBlob = 0xabb29bd49a2833fau,
+                                HHDM = 0xb0ed257db18cb58f,
+                        };
+                }
+
+                using Tag = Stivale::Tag<TagId::Enum>;
+
+                struct Struct {
+                        static constexpr size_t
+                                BrandLength = 64u,
+                                VersionLength = 64u;
+
+                        char
+                                brand[BrandLength],
+                                version[VersionLength];
+                        Tag *tags;
+                };
+
+                struct PMR {
+                        enum Permission : uint64_t {
+                                Executable = 1u << 0u,
+                                Writable = 1u << 1u,
+                                Readable = 1u << 2u,
+                        };
+
+                        uintptr_t base;
+                        size_t length;
+                        uint64_t permissions;
+                };
+                struct PMRs : Tag {
+                        size_t length;
+                        PMR items[];
+                };
+
+                struct KernelBaseAddress : Tag {
+                        uintptr_t physicalBaseAddress;
+                        uintptr_t virtualBaseAddress;
+                };
+
+                struct CommandLine : Tag {
+                        uint64_t commandLine;
+                };
+
+                struct MemoryMapEntry {
+                        enum Type : uint32_t {
+                                Usable = 1,
+                                Reserved = 2,
+                                ACPIReclaimable = 3,
+                                ACPINvs = 4,
+                                BadMemory = 5,
+                                BootloaderReclaimable = 0x1000,
+                                Kernel = 0x1001,
+                                Framebuffer = 0x1002,
+                        };
+
+                        uintptr_t base;
+                        size_t length;
+                        Type type;
+                        uint32_t unused;
+                };
+
+                struct MemoryMap : Tag {
+                        size_t length;
+                        MemoryMapEntry items[];
+                };
+
+                struct Framebuffer : Tag {
+                        enum MemoryModel : uint8_t {
+                                RGB = 1u,
+                        };
+
+                        uint64_t framebufferAddress;
+                        uint16_t framebufferWidth;
+                        uint16_t framebufferHeight;
+                        uint16_t framebufferPitch;
+                        uint16_t framebufferBitsPerPixel;
+                        MemoryModel memoryModel;
+                        uint8_t redMaskSize;
+                        uint8_t redMaskShift;
+                        uint8_t greenMaskSize;
+                        uint8_t greenMaskShift;
+                        uint8_t blueMaskSize;
+                        uint8_t blueMaskShift;
+                        uint8_t unused;
+                };
+
+                struct EDID : Tag {
+                        size_t length;
+                        uint8_t data[];
+                };
+
+                struct TextMode : Tag {
+                        uintptr_t address;
+                        uint16_t unused;
+                        uint16_t rows;
+                        uint16_t cols;
+                        uint16_t bytesPerChar;
+                };
+
+                using MTRR = Tag;
+
+                struct Terminal : Tag {
+                        using TermWrite = void(const char *string, size_t length);
+
+                        uint32_t flags;
+                        uint16_t cols;
+                        uint16_t rows;
+                        TermWrite *write;
+                        uint64_t maxLength;
+                };
+
+                struct Module {
+                        static constexpr size_t StringSize = 128u;
+
+                        uintptr_t begin;
+                        uintptr_t end;
+                        char string[StringSize];
+                };
+                struct Modules : Tag {
+                        size_t length;
+                        Module items[];
+                };
+
+                struct RSDP : Tag {
+                        uint64_t rsdp;
+                };
+
+                struct Epoch : Tag {
+                        uint64_t epoch;
+                };
+
+                struct Firmware : Tag {
+                        enum Flags : uint64_t {
+                                UEFI = 0u,
+                                Bios,
+                        };
+
+                        Flags flags;
+                };
+
+                struct EFISystemTable : Tag {
+                        uintptr_t systemTable;
+                };
+
+                struct KernelFile : Tag {
+                        uintptr_t kernelFile;
+                };
+                struct KernelFile_v2 : Tag {
+                        uintptr_t kernelFile;
+                        size_t kernelSize;
+                };
+
+                struct GUID {
+                        uint32_t a;
+                        uint16_t b;
+                        uint16_t c;
+                        uint8_t d[8];
+                };
+                struct BootVolume : Tag {
+                        uint64_t flags;
+                        GUID guid;
+                        GUID part_guid;
+                };
+
+                struct KernelSlide : Tag {
+                        uint64_t kernelSlide;
+                };
+
+                struct SMBIOS : Tag {
+                        uint64_t flags;
+                        uint64_t smbiosEntry32;
+                        uint64_t smbiosEntry64;
+                };
+
+                struct SMPInfo {
+                        uint32_t processorId;
+                        uint32_t lapicId;
+                        uint64_t targetStack;
+                        uintptr_t gotoAddress;
+                        uint64_t extraArgument;
+                };
+                struct SMP : Tag {
+                        uint64_t flags;
+                        uint32_t bspLapicId;
+                        uint32_t unused;
+                        uint64_t cpuCount;
+                        SMPInfo smpInfo[];
+                };
+
+                struct PXEServerInfo : Tag {
+                        uint32_t serverIp;
+                };
+
+                struct MMIO32UART : Tag {
+                        uintptr_t address;
+                };
+
+                struct DeviceTreeBlob : Tag {
+                        uintptr_t address;
+                        size_t size;
+                };
+
+                struct HHDM : Tag {
+                        uintptr_t address;
+                };
+        }
+}
+
+#define STIVALE2_TERM_CTX_SIZE ((uint64_t)(-1))
+#define STIVALE2_TERM_CTX_SAVE ((uint64_t)(-2))
+#define STIVALE2_TERM_CTX_RESTORE ((uint64_t)(-3))
+#define STIVALE2_TERM_FULL_REFRESH ((uint64_t)(-4))
+
+#endif //_XOS_SRC_STIVALE2_H
